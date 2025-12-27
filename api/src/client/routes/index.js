bee46cc3a0b9a34ce1b38049624086b6a4a4d6ee
yo-dalo <kumaradarsh00572@gmail.com>
2025-12-27 23:48:11 +0530

    notification ooor massege backend ke connctet kr diya h

--- api/src/client/routes/index.js
+++ api/src/client/routes/index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const posterRoutes = require('./posterRoutes');
-const notificationRoutes = require('./notificationRoutes');
-const messagesRoutes = require('./messagesRoutes');
-const notificationRoutes = require('./notificationRoutes');
 const pagesRoutes = require('./pagesRoutes');
-const notificationRoutes = require('./notificationRoutes');
 const messagesRoutes = require('./messagesRoutes');
 const notificationRoutes = require('./notificationRoutes');
 
