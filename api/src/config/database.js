e2a0fd4278fd70b77eac0833f83c35f13c020721
yo-dalo <kumaradarsh00572@gmail.com>
2026-02-28 18:52:50 +0530

    .

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,16 +1,27 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 const env = require("./env");
+
+// const sequelize = new Sequelize(env.dbName, env.dbUser, env.dbPass, {
+//   host: env.dbHost,
+//   port: env.port,
+//   dialect: "mysql",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+//   logging: false
+// });
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(env.dbName, env.dbUser, env.dbPass, {
   host: env.dbHost,
-  port: env.port,
   dialect: "mysql",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
   logging: false
 });
 
