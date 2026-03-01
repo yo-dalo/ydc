85355e052d833f056c9efaea4e4c210b00b15443
yo-dalo <kumaradarsh00572@gmail.com>
2026-03-01 08:09:44 +0530

    dashboard route kam krne lga h

--- api/src/admin/routes/dash_boardRoutes.js
+++ api/src/admin/routes/dash_boardRoutes.js
@@ -4,9 +4,9 @@ const dash_boardController = require("../controllers/dash_boardController");
 
 
 router.get("/", dash_boardController.getAll);
-router.get("/:id", dash_boardController.getById);
-router.get("/for-update/:id", dash_boardController.getForUpdate);
+// router.get("/:id", dash_boardController.getById);
+// router.get("/for-update/:id", dash_boardController.getForUpdate);
 
-router.delete("/:id", dash_boardController.delete);
+// router.delete("/:id", dash_boardController.delete);
 
 module.exports = router;
