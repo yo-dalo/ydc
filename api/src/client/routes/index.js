701aeb4932e28c75ace9ec881b73b45378f131d0
yo-dalo <kumaradarsh00572@gmail.com>
2026-03-10 19:24:13 +0530

    quick linke add in clinet

--- api/src/client/routes/index.js
+++ api/src/client/routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const posterRoutes = require('./posterRoutes');
+
 const pagesRoutes = require('./pagesRoutes');
+
 const messagesRoutes = require('./messagesRoutes');
+
 const admissionRoutes = require('./admissionRoutes');
+
 const notificationRoutes = require('./notificationRoutes');
+
 const toperRoutes = require('./toperRoutes');
+const quick_linkRoutes = require('./quick_linkRoutes');
 const school_welcome_messageRoutes = require('./school_welcome_messageRoutes')
 const admission_open_messageRoutes = require('./admission_open_messageRoutes')
 const school_infoRoutes = require('./school_infoRoutes')
@@ -24,4 +30,5 @@ router.use('/toper', checkBranch, toperRoutes);
 router.use('/school-welcome-message', checkBranch, school_welcome_messageRoutes)
 router.use('/admission-open-message', checkBranch, admission_open_messageRoutes)
 router.use('/school-info', checkBranch, school_infoRoutes)
+router.use('/quick-link', quick_linkRoutes);
 module.exports = router;
